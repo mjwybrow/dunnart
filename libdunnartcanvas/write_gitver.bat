9939f1906248451ef403a371173df14c7b405edb
Michael Wybrow <mjwybrow@samediff.net>
2011-08-29 13:32:56 +1000

    Fix .bat file that generates githash.h on Windows.

--- libdunnartcanvas/write_gitver.bat
+++ libdunnartcanvas/write_gitver.bat
@@ -3,7 +3,7 @@ set PATH=%PATH%;"C:\Program Files\Git\bin"
 
 git show --abbrev-commit | grep "^commit" | cut -f2 -d' ' > value
 COPY /Y .init+value initvar.bat
-initvar.bat 
+call initvar.bat 
 
 echo #define GITHASH "%GITVER%" > %1
 DEL /Q value initvar.bat
