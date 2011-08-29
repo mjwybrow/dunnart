a62e2e0c332f50f49113869af6fe652c7a467ce7
Michael Wybrow <mjwybrow@samediff.net>
2011-08-29 11:50:36 +1000

    Update from Adaptagrams repository.

--- libavoid/debug.h
+++ libavoid/debug.h
@@ -48,7 +48,6 @@ namespace Avoid {
 
   #ifdef _MSC_VER
     // Compiling with Microsoft Visual C++ compiler
-
     #define db_printf  ATL::AtlTrace
   #else
     inline void db_printf(const char *fmt, ...)
@@ -71,15 +70,8 @@ namespace Avoid {
 // err_printf are critical errors that mean something pretty bad has happened:
 #ifdef _MSC_VER
 
-  // For Microsoft Visual C++ compiler we only display them if LIBAVOID_DEBUG
-  // is defined.
-  #ifdef LIBAVOID_DEBUG
+    // Compiling with Microsoft Visual C++ compiler
     #define err_printf ATL::AtlTrace
-  #else
-    inline void err_printf(const char *, ...)
-    {
-    }
-  #endif
 
 #else
 
