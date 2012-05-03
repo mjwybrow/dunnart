812ee2356824b64aa8774eb1a20bfaaef46083f0
Michael Wybrow <mjwybrow@samediff.net>
2012-05-03 15:47:19 +1000

    Allow use of clang++ compiler on Mac.

--- qmake.sh
+++ qmake.sh
@@ -2,11 +2,13 @@
 
 # macx-llvm
 # macx-g++42
-# ~/mkspecs/unsupported/macx-clang
 
 if [ "$1" == "clang" ]; then
 
-	qmake -spec ~/mkspecs/unsupported/macx-clang -recursive -config "$2" *.pro
+	qmake -spec unsupported/macx-clang -recursive -config "$2" *.pro
+	
+	# There is some clang linking bug that causes issues with OGDF,
+	# so use GCC for that instead.
 	(
 	cd libogdf
 	qmake -spec macx-g++42 -recursive -config "$2" *.pro
