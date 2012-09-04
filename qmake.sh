f9f4076519f4414bf9a0f20cea454d085a4e287e
Michael Wybrow <mjwybrow@samediff.net>
2012-09-04 14:14:02 +1000

    Clang++ can now compile the whole project.

--- qmake.sh
+++ qmake.sh
@@ -4,17 +4,9 @@
 # macx-g++42
 
 if [ "$1" == "clang" ]; then
-
 	qmake -spec unsupported/macx-clang -recursive -config "$2" *.pro
-	
-	# There is some clang linking bug that causes issues with OGDF,
-	# so use GCC for that instead.
-	(
-	cd libogdf
-	qmake -spec macx-g++42 -recursive -config "$2" *.pro
-	)
 else
-	qmake -spec macx-g++42 -recursive -config "$@" *.pro
+	qmake -spec macx-g++ -recursive -config "$@" *.pro
 fi
 
 
