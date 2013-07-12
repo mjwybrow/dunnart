e55545679b4968929d4f0abefef04576d60b5137
Michael Wybrow <mjwybrow@samediff.net>
2013-07-12 14:48:48 +0200

    Use correct qmake clang spec file for Qt 5.1.

--- qmake.sh
+++ qmake.sh
@@ -5,7 +5,7 @@
 
 
 if [ "$1" == "clang" ]; then
-	qmake -spec macx-clang-libc++ -recursive -config "$2 usemactoolbar" *.pro
+	qmake -spec macx-clang -recursive -config "$2 usemactoolbar" *.pro
 else
 	qmake -spec macx-g++ -recursive -config "$@" *.pro
 fi
